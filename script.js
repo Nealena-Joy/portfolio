7b4ab211fc9b5ef4b186ea259b72ff70230c2389
Nealena Ras <nealenaras@gmail.com>
2021-10-08 08:05:07 -0400

    second

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+// Toggle Email Form
+function toggleEmailForm(){
+    var x = document.getElementById("emailForm");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+};
+
+
