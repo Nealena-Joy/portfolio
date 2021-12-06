1c8f14d25b07f45627032d8ade7e38ce7b3b38fa
Nealena Ras <nealenaras@gmail.com>
2021-12-05 20:06:42 -0500

    update blue and red badge

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // Toggle Email Form
 function toggleEmailForm(){
     var x = document.getElementById("emailForm");
@@ -9,5 +7,3 @@ function toggleEmailForm(){
         x.style.display = "block";
     }
 };
-
-
